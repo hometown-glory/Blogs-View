fe02b229aee7c5093cfe1de282bee8834a2cbb12
admin <1783736194@qq.com>
2024-10-27 10:24:21 +0800

    完成博客前端项目首页导航栏开发

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+      <router-link to="/"></router-link>
+<router-view />
 </template>
 
 <style>
@@ -28,3 +25,5 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script setup lang="ts">
+</script>
