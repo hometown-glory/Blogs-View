7492bf9e7a0a6ef471f55d44d99ed2e90187ce3e
admin <1783736194@qq.com>
2024-11-01 14:28:36 +0800

    完成博客前端项目后台底部导航栏实现

--- src/views/admin/components/adminFooter.vue
+++ src/views/admin/components/adminFooter.vue
@@ -4,8 +4,9 @@
 </script>
 
 <template>
-  <div class="bg-cyan-700 text-white">
-    底部
+  <div class="bg-white py-5 flex items-center justify-center text-sm text-gray-500 shadow-none">
+    <!-- Copyright 版权信息 -->
+    Copyright © 2023. All rights reserved. Provided by&nbsp; <a class="underline" href="https://www.quanxiaoha.com" target="_blank">呆溟的博客</a>
   </div>
 </template>
 
